5504bc3ad99088062d8bc4fd3008fe0c89f1fd16
chandiis <ckaur05@hotmail.com>
2025-12-09 16:03:20 +0100

    Update functions.c

--- functions.c
+++ functions.c
@@ -4,6 +4,7 @@
 #include "spi.h"
 #include "FreeRTOS.h"
 #include "gpio.h"
+#include "tasks.h"
 #include <stdbool.h>
 #include "leds.h"
 
@@ -21,11 +22,6 @@ void ShiftReg_SendBytes(uint8_t data[], uint16_t length)
     HAL_GPIO_WritePin(STCP_GPIO_Port, STCP_Pin, GPIO_PIN_RESET);
 }
 
-/*void initialization(){
-	uint8_t leds[3] = {0b00100100, 0b01001100, 0b01001100};
-    ShiftReg_SendBytes(leds, 3);
-}*/
-
 bool PL1_Hit(void) //SW5 and SW6
 { if (HAL_GPIO_ReadPin(PL1_Switch_GPIO_Port, PL1_Switch_Pin) == 0) return true;
 else return false;
@@ -36,10 +32,10 @@ bool PL2_Hit(void) //SW7 and SW8
 else return false;
 }
 
-/*bool SW1_Hit(void) //SW7 and SW8
+bool SW1_Hit(void) //SW7 and SW8
 { if (HAL_GPIO_ReadPin(TL1_Car_GPIO_Port, TL1_Car_Pin) == 0) return true;
 else return false;
-}*/
+}
 
 void set(uint8_t leds[], const defLED *led){
 	leds[led->shift_reg] = leds[led->shift_reg] | (1 << led->bits);
@@ -47,20 +43,58 @@ void set(uint8_t leds[], const defLED *led){
 	ShiftReg_SendBytes(leds, 3);
 }
 
+void reset(uint8_t leds[], const defLED *led){
+	leds[led->shift_reg] = leds[led->shift_reg] & ~(1 << led->bits); //~ = bitwise NOT
+	ShiftReg_SendBytes(leds, 3);
+}
+
+void toggleLED(uint8_t leds[], const defLED *led){
+	static bool state = false; //state for the LED
+
+	if(state){
+		//Turn off the LED
+		reset(leds, led); //~ = bitwise NOT
+		HAL_Delay(100);
+	} else {
+		//Turn on the LED
+		set(leds, led);
+		HAL_Delay(100);
+	}
+	state = !state; //flip the state for the next call
+}
+
+/*bool PL1_Green_IsOn(void)
+{
+    // Check if the bit for PL1 green LED is set in leds[2]
+    if (leds[PL1_Green.shift_reg] & (1 << PL1_Green.bits))
+    {
+        return true;   // LED is ON
+    }
+    else
+    {
+        return false;  // LED is OFF
+    }
+}*/
+
 void initialization(){
+
+	uint8_t leds[3] = {0b0,0b0,0b0};
+	bool ped_request1 = false;
+	bool ped_request2 = false;
+
 	while(1){
-		if(PL1_Hit()){ //if SW5 or SW6 are pressed
-			uint8_t leds[3] = {0b0,0b0,0b0};
-			HAL_Delay(toggleFreq);
-			set(leds, &PL1_Green);
+
+		// Knappar
+		if (PL2_Hit()) {
+			toggleLED(leds, &PL2_Blue);
 		}
-		if(PL2_Hit()){ //if SW7 or SW8 are pressed
-			uint8_t leds[3] = {0b0,0b0,0b0};
-			HAL_Delay(toggleFreq);
-			set(leds, &PL2_Green);
-		}
+
+
 		else{
-			uint8_t leds[3] = {0b00100100, 0b01001100, 0b01001100};
+		    leds[0] = 0b00100100;
+		    leds[1] =  0b01001100;
+		    leds[2] = 0b01001100;
+
 			ShiftReg_SendBytes(leds, 3);
 		}
 	}
